34c51129777eb55bd89b6d5e295a857ca69d5684
Henry Ecker <hecker@umass.edu>
2016-11-08 18:55:26 -0500

    fb6

--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -2,16 +2,61 @@ import React from 'react';
 import StatusUpdate from './statusupdate';
 import CommentThread from './commentthread';
 import Comment from './comment';
+import {postComment, likeFeedItem, unlikeFeedItem} from './../server';
 
 export default class FeedItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = props.data;
+    }
+
+    handleCommentPost(commentText) {
+        postComment(this.state._id, 4, commentText, (updatedFeedItem) => {
+            this.setState(updatedFeedItem);
+        });
+    }
+
+    handleLikeClick(clickEvent) {
+        clickEvent.preventDefault();
+        if (clickEvent.button === 0) {
+            var callbackFunction = (updatedLikeCounter) => {
+                this.setState({likeCounter: updatedLikeCounter});
+            };
+            if (this.didUserLike()) {
+                unlikeFeedItem(this.state._id, 4, callbackFunction);
+            } else {
+                likeFeedItem(this.state._id, 4, callbackFunction);
+            }
+        }
+    }
+    didUserLike() {
+        var likeCounter = this.state.likeCounter;
+        var liked = false;
+        for (var i = 0; i < likeCounter.length; i++) {
+            if (likeCounter[i]._id === 4) {
+                liked = true;
+                break;
+            }
+        }
+        return liked;
+    }
+
     render() {
-        var data = this.props.data;
+        var data = this.state;
         var contents;
+        var likeButtonText = "Like";
+        if (this.didUserLike()) {
+            likeButtonText = "Unlike";
+        }
         switch (data.type) {
             case "statusUpdate":
                 contents = (
                     <StatusUpdate key={data._id} author={data.contents.author} postDate={data.contents.postDate} location={data.contents.location}>
-                        {data.contents.contents}
+                        {data.contents.contents.split("\n").map((line, i) => {
+                            return (
+                                <p key={"line" + i}>{line}</p>
+                            );
+                        })}
                     </StatusUpdate>
                 );
                 break;
@@ -27,9 +72,11 @@ export default class FeedItem extends React.Component {
                         <div className="col-md-12">
                             <ul className="list-inline">
                                 <li>
-                                    <a href="#">
+                                    <a href="#" onClick= { (e) => this.handleLikeClick(e) }>
                                         <span className="glyphicon glyphicon-thumbs-up"></span>
-                                        Like</a>
+                                        {likeButtonText
+}
+                                    </a>
                                 </li>
                                 <li>
                                     <a href="#">
@@ -49,12 +96,12 @@ export default class FeedItem extends React.Component {
                 <div className="panel-footer">
                     <div className="row">
                         <div className="col-md-12">
-                            <a href="#">{data.likeCounter.length} people </a>
+                            <a href="#">{data.likeCounter.length + " people "}</a>
                             like this
                         </div>
                     </div>
                     <hr/>
-                    <CommentThread>
+                    <CommentThread onPost={(commentText) => this.handleCommentPost(commentText)}>
                         {data.comments.map((comment, i) => {
                             return (
                                 <Comment key={i} author={comment.author} postDate={comment.postDate}>
@@ -62,9 +109,9 @@ export default class FeedItem extends React.Component {
                                 </Comment>
                             );
                         })}
-                   </CommentThread>
+                    </CommentThread>
                 </div >
-              </div>
+            </div>
         )
     }
 }
